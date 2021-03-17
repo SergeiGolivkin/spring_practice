ddc68d948afb111a49a6692ce1881631bff208c6
Sergey <golivkin.s.v@gmail.com>
2021-03-17 09:23:59 +0300

    OneToOne

---
+++ src/hibernate_test2/Test2.java
@@ -0,0 +1,39 @@
+package hibernate_test2;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+            session = factory.getCurrentSession();
+            Employee employee = new Employee("Olga", "Sidorova", "HR", 779);
+            Detail detail = new Detail("New York", "+19186888634", "olga@gmail.com");
+
+            employee.setEmDetail(detail);
+
+            session.beginTransaction();
+
+            session.save(employee);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+
+    }
+}
